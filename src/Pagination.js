264d534096f6d21f516bef36b42426cc801486b9
omerenma <godwin2341@gmail.com>
2021-08-02 11:24:41 +0100

    dashbaord component

---
+++ src/Pagination.js
@@ -0,0 +1,9 @@
+ import React,{useMemo} from "react";
+ export const Pagination = ({
+     totalCount, 
+     currentPage,
+     pageSize,
+    sublingCount = 1
+ }) => {
+  return <h1>Pagination</h1>;
+};
